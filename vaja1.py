df748c661e4af82323699b8d37ce7a8147a8ac8a
BenjoDev <benjamintrzan@protonmail.com>
2023-03-11 14:35:27 +0100

    dokoncano dolocanje barve koze

--- vaja1.py
+++ vaja1.py
@@ -4,7 +4,29 @@ import matplotlib.pyplot as plt
 
 def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj):
     print(str(levo_zgoraj[0]) + "\n" + str(levo_zgoraj[1]) + "\n" + str(desno_spodaj[0]) + "\n" + str(desno_spodaj[1]) + "\n")
-    # return (spodnja_meja_koze,zgornja_meja_koze)
+
+    spodnja_meja_koze = np.ndarray((1,3))
+    zgornja_meja_koze = np.ndarray((1,3))
+
+    for c in range(3):
+        tmp = slika[:,:,c]
+        vsota = 0
+        for i in tmp:
+            for j in i:
+                vsota = vsota + j
+        vsota = (vsota / len(tmp)) / len(tmp[0])
+        # print(vsota)
+
+        std_odklon = 0
+        for i in tmp:
+            std_odklon = std_odklon + np.std(i, ddof=1)
+        std_odklon = std_odklon / len(tmp)
+        # print(std_odklon)
+
+        spodnja_meja_koze[0][c] = int(vsota - std_odklon) # blue, green, red
+        zgornja_meja_koze[0][c] = int(vsota + std_odklon)
+
+    return (spodnja_meja_koze,zgornja_meja_koze)
 
 def zmanjsaj_sliko(slika):
     pass
@@ -30,7 +52,15 @@ while True:
             cv2.destroyWindow("Kamera")
             r = cv2.selectROI("Izberi barvo koze", frame)
             cv2.destroyWindow("Izberi barvo koze")
-            doloci_barvo_koze(frame, [r[0], r[1]], [r[0] + r[2], r[1] + r[3]]) # x in y koordinata levo zgoraj, x + širina in y + višina desno spodaj 
+
+            izbrano_obmocje = frame[r[1]:r[1]+r[3], r[0]:r[0]+r[2]]
+
+            # x in y koordinata levo zgoraj, x + širina in y + višina desno spodaj 
+            barva_koze = doloci_barvo_koze(izbrano_obmocje, [r[0], r[1]], [r[0] + r[2], r[1] + r[3]]) 
+
+            print(barva_koze[0])
+            print(barva_koze[1])
+
         cv2.imshow("Kamera",frame)
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
